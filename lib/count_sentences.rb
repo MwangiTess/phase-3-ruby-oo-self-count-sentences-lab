cec1abaf15159f470eb79f682aa0936612266f17
Teresia <teresiamwangi26@gmail.com>
2023-05-20 17:53:48 +0300

    Complete ASsignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+word = ("The weather outside is frightful.")
+puts word.sentence?
+word1 = ("The weather outside is frightful?")
+puts word1.question?
+word2 = ("The weather outside is frightful!")
+puts word2.exclamation?
+word4 = ("!?.")
+word5 = ("The, weather, outside, is, great,")
+puts word4.count_sentences
+puts word5.count_sentences
+
+#binding.pry
